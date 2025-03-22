163d0cb5b6203ab7769dfb2c4b51f858bba20379
Tonkaat <jdalmendras05@gmail.com>
2025-03-22 18:05:18 +0800

    Updated files with new changes

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const app = express()
 app.set("view engine", "ejs");
 app.set("views", "./views");
 const port = 4000;
+const axios = require("axios");
 
 const userRouter = require("./routers/users");
 app.use("/users", userRouter);
 
-app.get("/", (req, res) => {
-  res.redirect("/users");
+app.get("/", async (req, res) => {
+  try {
+    const response = await
+axios.get('https://jsonplaceholder.typicode.com/posts');
+    res.render('index', {posts: response.data});
+  } catch (error) {
+    res.status(500).send('Error fectching data');
+  }
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
