9007421937ed51bae5285e59c00766f1a9dc383f
Tonkaat <jdalmendras05@gmail.com>
2025-04-26 17:02:17 +0800

    Updated files with new changes

--- server.js
+++ server.js
@@ -1,60 +1,67 @@
 const express = require("express");
 const app = express();
+const db = require("./db");
 
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: true }));
 app.use(express.static("public"));
 
-const port = 4000;
+const PORT = 4000;
 
-app.use(express.json()); // middleware  
+ 
 
-let users = [
-  { id: 1, name: "Richard Majos", email: "richard@gmail.com" },
-  { id: 2, name: "Jaypi Yazimerz", email: "jaypi@gmail.com" },
-  { id: 3, name: "Mark Escober", email: "mark@gmail.com" },
-];
-
+// GET: All users
 app.get("/users", (req, res) => {
+  const users = db.prepare("SELECT * FROM users").all();
   res.render("index", { users });
 });
 
+// GET: New user form
 app.get("/users/new", (req, res) => {
   res.render("new");
 });
 
+// POST: Create new user
 app.post("/users", (req, res) => {
   const { name, email } = req.body;
-  const newUser = { id: Date.now(), name, email};
-
-  users.push(newUser);
+  db.prepare("INSERT INTO users (name, email) VALUES (?, ?)").run(name, email);
   res.redirect("/users");
 });
 
+// GET: Edit user form
 app.get("/users/:id/edit", (req, res) => {
-  const user = users.find((u) => u.id == req.params.id);
+  const user = db
+    .prepare("SELECT * FROM users WHERE id = ?")
+    .get(req.params.id);
   res.render("edit", { user });
 });
 
+// POST: Update user
 app.post("/users/:id", (req, res) => {
   const { name, email } = req.body;
-  const user = users.find((u) => u.id == req.params.id);
-  if (user) {
-    user.name = name;
-    user.email = email;
-  }
+  db.prepare("UPDATE users SET name = ?, email = ? WHERE id = ?").run(
+    name,
+    email,
+    req.params.id
+  );
   res.redirect("/users");
 });
 
+// POST: Delete user
 app.post("/users/:id/delete", (req, res) => {
-  users = users.filter((u) => u.id != req.params.id);
+  db.prepare("DELETE FROM users WHERE id = ?").run(req.params.id);
   res.redirect("/users");
 });
 
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+//to check your db's content via the web
+app.get("/debug", (req, res) => {
+  const users = db.prepare("SELECT * FROM users").all();
+  res.json(users);
 });
 
 
